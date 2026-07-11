74dae8f054cff5817b5cf41279821b4cb79ddc2d
quangmingdoc <nguyenqminh16@gmail.com>
2026-07-11 20:00:16 +0700

    feat: establish MySQL connection for contract service with logging

--- backend/src/modules/contracts/contract.service.js
+++ backend/src/modules/contracts/contract.service.js
@@ -1,4 +1,20 @@
-const { conn } = require('../../../server');
+const mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456",
+    database: "quan_ly_tro"
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.log("Loi ket noi MySQL (contract module):", err);
+    } else {
+        console.log("Contract module: Ket noi MySQL thanh cong");
+    }
+});
 
 const getAllContracts = (callback) => {
   const sql = `
